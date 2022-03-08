dcda6021c5b97dbd715c014764cceb58e940dd76
rafaelmoczalla <Rafael.Moczalla@hpi.de>
2022-03-08 02:55:40 +0100

    listing free slots for Velodrom

--- App.tsx
+++ App.tsx
@@ -2,56 +2,107 @@ import { useEffect, useState, useCallback } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, FlatList, View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-  
-  const [text, onChangeText] = useState('Wrtie some Text');
-  const [number, onChangeNumber] = useState('');
 
   const styles = StyleSheet.create({
     input: {
       height: 40,
-      margin: 12,
+      margin: 6,
       borderWidth: 1,
       padding: 10,
     },
   });
 
-  const [badText, setBadText] = useState("Waiting...");
+  const Item = ({text = 'Item'}) => (
+    <View>
+      <Text style={styles.input}>{text}</Text>
+    </View>
+  );
+
+  const renderItem = ({item = 'renderItem'}) => (
+    <Item text={item} />
+  );
+
+  const [textList, setTextList] = useState(["Waiting..."]);
   
   // Read text from URL location
-  const url = 'https://www.berlinerbaeder.de/baeder/bad-suche/#/';
+  const url = 'https://pretix.eu/Baeder/';
+  
+  let bathNrs: string[];
+  bathNrs = [ '79' ];
+
+  //for (var i = 1; i < 180; ++i) {
+  //  bathNrs.push(i.toString());
+  //}
+    //'1', '2', '7', '9', '11', '15', '17', '18', '19', '21', '24', '26', '27', '28', '29',
+    //'30', '31', '34', '38', '42', '43', '45', '46', '47', '48', '49', '51', '52', '54', '60',
+    //'61', '62', '64', ];
   
   const makeAPICall = useCallback(async () => {
-    await fetch('https://cors-anywhere.herokuapp.com/' + url, {
-      method: 'GET'
-    })
-    .then(response => {
-      if (response.ok) {
-        console.log(response);
-        response.text().then(text => {
-          var parser = new DOMParser();
-        	var list = parser.parseFromString(text, 'text/html').getElementById('filter');
-          if (list !== null) {
-            var bad = list.getElementsByTagName('li');
-            console.log(bad);
-            setBadText(bad[0].innerText);
-          }
-          else {
-            console.log('Empty pool list.');
-          }
-        });
-      } else {
-        console.log('failed');
+    do {
+      var fetches: any[] = [];
+
+      for (var ib in bathNrs) {
+        fetches.push(
+          fetch(url + bathNrs[ib] + '/', {
+            method: 'GET'
+          }).then(response => {
+            if (response.ok) {
+              return response.text();
+            } else {
+              console.error('Cannot communicate with website.');
+            }
+
+            return 'failed';
+          }).then(text => {
+            if (text !== 'failed') {
+              var parser = new DOMParser();
+              var doc = parser.parseFromString(text, 'text/html');
+              var list = doc.getElementsByClassName('event  available');
+              if (list !== null) {
+                var bath = doc.getElementsByClassName('content-header');
+                var name = "Missing Name";
+                if (bath[0].textContent !== null) {
+                  name = bath[0].textContent.trim();
+                }
+                
+                var out: string[] = [name];
+                
+                for(var i = 0; i < list.length; ++i) {
+                  var time = list[i].querySelector('time');
+                  if (time !== null) {
+                    out.push(time.innerText);
+                  }
+                }
+                
+                return(out);
+              }
+            }
+          })
+        );
       }
-    });
+
+      Promise.all(fetches).then(function(list) {
+        var outList: string[] = [];
+        
+        for (var strs in list) {
+          if (list[strs] !== 'no slots free' && list[strs] !== 'failed') {
+            for (var str in list[strs]) {
+              outList.push(list[strs][str]);
+            }
+          }
+        }
+        
+        setTextList(outList);
+      });
+
+      await new Promise(f => setTimeout(f, 60000));
+    } while (true);
   }, []);
 
   // Send an async HTTP Get request to the url
@@ -65,11 +116,8 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
         <StatusBar />
-        <Text style={styles.input}>
-          {badText}
-        </Text>
+        <FlatList data={textList} renderItem={renderItem}/>
       </SafeAreaProvider>
     );
   }
